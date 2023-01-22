6f6d16dd1995b66fc0cc7b1a422a7dc958f92afe
Ryan Zeigler <ryan.zeigler@s5stratos.com>
2023-01-22 07:59:06 -0600

    Now with tokyonight

--- init.lua
+++ init.lua
@@ -2,8 +2,7 @@ require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use 'folke/tokyonight.nvim'
-  use 'ellisonleao/gruvbox.nvim'
-  use 'sainnhe/edge'
+  use 'lifepillar/vim-solarized8'
 
   use 'feline-nvim/feline.nvim'
 
@@ -170,7 +169,7 @@ vim.g.mapleader = ','
 
 vim.o.background='dark'
 vim.cmd('set termguicolors')
-vim.cmd('colorscheme gruvbox')
+vim.cmd('colorscheme tokyonight')
 vim.cmd('set number')
 vim.cmd('set expandtab shiftwidth=2 tabstop=2')
 
