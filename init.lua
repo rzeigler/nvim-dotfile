0e014e3ca2979e4ed71644fd7aa07514c3213832
Ryan Zeigler <ryan.zeigler@s5stratos.com>
2023-01-26 09:02:22 -0600

    I really do like gruvbox

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
-  use 'EdenEast/nightfox.nvim'
+  use { "ellisonleao/gruvbox.nvim" }
+
   use 'godlygeek/tabular'
 
   use 'ixru/nvim-markdown'
@@ -25,12 +26,48 @@ require('packer').startup(function(use)
   use 'tpope/vim-vinegar'
 
   use {
-    'tanvirtin/vgit.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    },
+    'lewis6991/gitsigns.nvim',
     config = function()
-      require('vgit').setup()
+      require('gitsigns').setup({
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          -- Actions
+          map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+          map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
+
+          -- Text object
+          map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
+      })
     end
   }
 
@@ -237,13 +274,13 @@ vim.g.sonokai_style = 'andromeda'
 
 vim.o.background='dark'
 vim.cmd('set termguicolors')
-vim.cmd('colorscheme nightfox')
+vim.cmd('colorscheme gruvbox')
 vim.cmd('set number')
 vim.cmd('set expandtab shiftwidth=2 tabstop=2')
 
 require('lualine').setup({
   options = {
-    theme = 'tokyonight'
+    theme = 'gruvbox'
   },
   sections = {
     lualine_c = {
@@ -285,10 +322,6 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
--- Git
-vim.keymap.set('n', '[gh', require'vgit'.hunk_up, opts)
-vim.keymap.set('n', ']gh', require'vgit'.hunk_down, opts)
 
 -- LSP Telescope
 vim.keymap.set('n', '<leader>fr', builtin.lsp_references, opts)
