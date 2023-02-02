7c55c4462c8a23a80d1cc20a43c11bbab9d70d2f
Ryan Zeigler <ryan.zeigler@s5stratos.com>
2023-02-02 14:07:55 -0600

    Hardcode a base dir for jdtls

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,13 +1,40 @@
-local my_lsp = require('my_lsp')
+local util = require'lspconfig.util'
 
 local jdtls = require('jdtls')
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+local my_lsp = require('my_lsp')
+
+local env = {
+  HOME = vim.loop.os_homedir(),
+  XDG_CACHE_HOME = os.getenv 'XDG_CACHE_HOME',
+  JDTLS_JVM_ARGS = os.getenv 'JDTLS_JVM_ARGS',
+}
+
+local function get_cache_dir()
+  return env.XDG_CACHE_HOME and env.XDG_CACHE_HOME or util.path.join(env.HOME, '.cache')
+end
+
+local function get_jdtls_cache_dir()
+  return util.path.join(get_cache_dir(), 'jdtls')
+end
+
+local function get_jdtls_config_dir()
+  return util.path.join(get_jdtls_cache_dir(), 'config')
+end
+
+local function get_jdtls_workspace_dir()
+  return util.path.join(get_jdtls_cache_dir(), 'workspace')
+end
+
+local root_dir = vim.fs.dirname(vim.fs.find({'.gradlew', '.git', 'mvnw'}, { upward = true })[1])
+local workspace_root = root_dir:gsub('\\/', '_')
+
 local config = {
     -- cmd = {'/path/to/jdt-language-server/bin/jdtls'},
-    cmd = { 'jdtls' },
-    root_dir = vim.fs.dirname(vim.fs.find({'.gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+    root_dir = root_dir,
+    cmd = { 'jdtls', '--data', get_jdtls_workspace_dir() .. workspace_root },
     on_attach = function (client, bufnr)
       local bufopts = { noremap = true, silent=true, buffer=bufnr }
       my_lsp.on_attach(client, bufnr)
